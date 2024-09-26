250f48cc272611552e21493e7ef856edc679d580
Corrinedev <corbinsmithtx21@gmail.com>
2024-09-26 08:50:53 -0500

    commit

--- src/main/java/com/corrinedev/lostcitieshelper/ButtonController.java
+++ src/main/java/com/corrinedev/lostcitieshelper/ButtonController.java
@@ -3,13 +3,13 @@ package com.corrinedev.lostcitieshelper;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class ButtonController {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Created Parts/Palletes");
         FileSplitter.extractPallete();
 
     }
--- src/main/java/com/corrinedev/lostcitieshelper/FileSplitter.java
+++ src/main/java/com/corrinedev/lostcitieshelper/FileSplitter.java
@@ -1,6 +1,7 @@
 package com.corrinedev.lostcitieshelper;
 
 import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 
 import javax.swing.*;
 import java.io.File;
@@ -8,15 +9,32 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+
 public class FileSplitter {
 
     public static void extractPallete() {
+        JOptionPane.showMessageDialog(null,"Choose your lostcities directory, this is NOT the same directory as your file, its the directory your datapack is in, it should go like this ~/lostcities/lostcities");
+        DirectoryChooser directoryChooser = new DirectoryChooser();
 
-        DirectoryChooser fileChooser = new DirectoryChooser();
-
-        String directory = String.valueOf(fileChooser.showDialog(null));
+        String directory = String.valueOf(directoryChooser.showDialog(null));
 
         System.out.println(directory);
+
+        if(!(directory == null)) {
+
+            FileChooser fileChooser = new FileChooser();
+            File file = fileChooser.showOpenDialog(null);
+            String filePath = file.getAbsolutePath();
+            System.out.println(filePath);
+            String fileContents = null;
+            try {
+                fileContents = Files.readString(Path.of(filePath));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(fileContents);
+
+        }
 
 
     }
