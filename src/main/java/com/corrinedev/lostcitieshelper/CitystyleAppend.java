bbba31d419a8a785c833430a3c7181c35a19c50c
Corrinedev <corbinsmithtx21@gmail.com>
2024-09-28 14:38:07 -0500

    added factor variable

--- src/main/java/com/corrinedev/lostcitieshelper/CitystyleAppend.java
+++ src/main/java/com/corrinedev/lostcitieshelper/CitystyleAppend.java
@@ -21,19 +21,25 @@ public class CitystyleAppend {
 
 
         try {
-
+            float factor = 1;
             String fileContents = Files.readString(Path.of(cityStyle.getAbsolutePath()));
+            try {
+                factor = Float.parseFloat(JOptionPane.showInputDialog(null, "Input the NUMBER for your factor, it can be a decimal"));
+            } catch(Exception e) {
+                System.out.println("That is NOT a float number");
+                JOptionPane.showMessageDialog(null,"That is NOT a decimal, defaulting to a value of 1");
+            }
             String citystylepart1 = fileContents.substring(fileContents.indexOf("{"), fileContents.indexOf("\"buildings\": ["));
             String citystylepart2 = fileContents.substring(fileContents.indexOf("            {\n" +
                     "                \"factor\":"), fileContents.length());
 
             System.out.println(citystylepart1 + "\"buildings\": [" + "            {\n" +
-                    "                \"factor\": 0.3,\n" +
+                    "                \"factor\": " + factor +",\n" +
                     "                \"value\": \"" + buildingname + "\"\n" +
                     "            }," + citystylepart2);
 
             String citystylefinal = citystylepart1 + "\"buildings\": [" + "            {\n" +
-                    "                \"factor\": 0.3,\n" +
+                    "                \"factor\": " + factor + ",\n" +
                     "                \"value\": \"" + buildingname + "\"\n" +
                     "            }," + citystylepart2;
             Files.writeString(Path.of(cityStyle.getAbsolutePath()), citystylefinal);
